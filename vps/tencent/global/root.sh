26d92b34711e073384d1465f017a813e2a1bf745
yanghao5 <yanghao.buffer@outlook.com>
2025-10-27 20:20:32 +0800

    202510272020

--- vps/tencent/global/root.sh
+++ vps/tencent/global/root.sh
@@ -7,12 +7,14 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # swap mem 4G
-dd if=/dev/zero of=/swapfile bs=1G count=4
+sudo dd if=/dev/zero of=/swapfile bs=128M count=32
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 sysctl vm.swappiness=90
 
+# init
+apt-get update && apt-get install git wget curl -y
 
 # install 
 apt-get update && apt-get install sudo wget curl neovim git btop zsh rsync jq -y
@@ -21,6 +23,7 @@ apt-get update && apt-get install sudo wget curl neovim git btop zsh rsync jq -y
 adduser --disabled-password --gecos "" momo
 adduser --disabled-password --gecos "" lain
 adduser --disabled-password --gecos "" jumper
+
 # 给 root 权限
 usermod -aG sudo momo
 echo "%sudo   ALL=(ALL:ALL) ALL" | tee -a /etc/sudoers
@@ -29,61 +32,9 @@ echo "%sudo   ALL=(ALL:ALL) ALL" | tee -a /etc/sudoers
 echo "root:op" | chpasswd
 echo "momo:op" | chpasswd
 
-# SHH
-echo "Port 80" | tee -a /etc/ssh/sshd_config
-echo "PubkeyAuthentication yes" | tee -a /etc/ssh/sshd_config
-echo "AuthorizedKeysFile .ssh/authorized_keys" | tee -a /etc/ssh/sshd_config
-echo "Match User lain" | tee -a /etc/ssh/sshd_config
-echo "  PasswordAuthentication yes" | tee -a /etc/ssh/sshd_config
-sudo systemctl restart sshd
-
-## momo ssh key
-mkdir -p /home/momo/.ssh
-cp /root/.ssh/authorized_keys /home/momo/.ssh
-chown -R momo:momo /home/momo/.ssh
-chmod 700 /home/momo/.ssh
-chmod 600 /home/momo/.ssh/authorized_keys
-
-## jumper ssh key
-mkdir -p /home/jumper/.ssh
-cp /root/.ssh/authorized_keys /home/jumper/.ssh
-chown -R jumper:jumper /home/jumper/.ssh
-chmod 700 /home/jumper/.ssh
-chmod 600 /home/jumper/.ssh/authorized_keys
-
 # Docker
+curl -o get-docker.sh https://get.docker.com 
 sh ./get-docker.sh
 
 ## 给 momo 用户执行 docker 权限
 usermod -aG docker momo
-
-# install files
-
-# shell scripts
-chmod +x restore.sh user.sh
-chown momo:momo restore.sh user.sh
-mv restore.sh /home/momo
-mv user.sh /home/momo
-
-#ohmyzsh
-chmod +x ohmyzsh_install.sh
-chown momo:momo ohmyzsh_install.sh
-mv ohmyzsh_install.sh /home/momo
-
-#nvm
-chmod +x nvm_install.sh
-chown momo:momo nvm_install.sh
-mv nvm_install.sh /home/momo
-
-# Cmake
-chown momo:momo cmake-3.28.2-linux-x86_64.tar.gz
-mv cmake-3.28.2-linux-x86_64.tar.gz /home/momo
-
-# LLVM
-chown momo:momo llvm.sh
-chmod +x llvm.sh
-mv llvm.sh /home/momo
-
-# Golang
-chown momo:momo go1.24.2.linux-amd64.tar.gz
-mv go1.24.2.linux-amd64.tar.gz /home/momo
