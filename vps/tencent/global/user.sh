a82c98b3519577f4a990022288019e02a2a8faf2
yanghao5 <yanghao.buffer@outlook.com>
2025-10-27 22:36:41 +0800

    update 20251027223640

--- vps/tencent/global/user.sh
+++ vps/tencent/global/user.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # ========================================== download ============================================= #
 download_file() {
@@ -16,8 +16,8 @@ download_file() {
 
     # 尝试下载，直到文件存在且非空
     while true; do
-        # 使用 curl 下载文件
-        curl -o "$output" "$url"
+        # 使用 wget 下载文件
+        wget -O "$output" "$url"
         
         # 如果下载成功且文件非空，退出循环
         if check_file; then
@@ -25,6 +25,58 @@ download_file() {
             break
         else
             echo "faild download or file is empty, and trying download again..."
+            # 如果文件存在，尝试删除
+            if [ -f "$output" ]; then
+                rm "$output"
+            fi
+            sleep 2  # 暂停 2 秒后再试
+        fi
+    done
+}
+
+download_file_hash() {
+    local url="$1"
+    local output="$2"
+    local expected_sha256="$3"
+    
+    # 检测文件是否存在且非空
+    check_file() {
+        if [ -s "$output" ]; then
+            return 0  # 文件存在且非空
+        else
+            return 1  # 文件不存在或为空
+        fi
+    }
+
+    check_sha256() {
+        local actual_sha256
+        actual_sha256=$(sha256sum "$output" | awk '{ print $1 }')  # 获取纯 hash 值
+        if [ "$actual_sha256" == "$expected_sha256" ]; then
+            return 0  # SHA-256 匹配
+        else
+            echo "Actual sha256: $actual_sha256"
+            echo "Expected sha256: $expected_sha256"
+            return 1  # SHA-256 不匹配
+        fi
+    }
+
+    # 尝试下载，直到文件存在且非空
+    while true; do
+        # 使用 wget 下载文件
+        wget -O "$output" "$url"
+        sleep 15
+        
+        # 如果下载成功且文件非空，退出循环
+        echo "Verifying file..."
+        if check_file && check_sha256; then
+            echo "file: $output download success!"
+            break
+        else
+            echo "faild download or file is empty, and trying download again..."
+            # 如果文件存在，尝试删除
+            if [ -f "$output" ]; then
+                rm "$output"
+            fi
             sleep 2  # 暂停 2 秒后再试
         fi
     done
@@ -38,12 +90,10 @@ chmod +x ohmyzsh_install.sh
 download_file "https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh" "nvm_install.sh"
 chmod +x nvm_install.sh
 
-# Golang 1.24.2
-download_file "https://go.dev/dl/go1.24.2.linux-amd64.tar.gz" "go1.24.2.linux-amd64.tar.gz"
+
+download_file_hash "https://go.dev/dl/go1.24.2.linux-amd64.tar.gz" "go1.24.2.linux-amd64.tar.gz" "68097bd680839cbc9d464a0edce4f7c333975e27a90246890e9f1078c7e702ad"
 
 # =========================== Install =========================================== #
 
 # go 1.24.2
-# sudo tar -C /usr/local -xzf go1.24.2.linux-amd64.tar.gz
-
-
+#sudo tar -C /usr/local -xzf go1.24.2.linux-amd64.tar.gz
