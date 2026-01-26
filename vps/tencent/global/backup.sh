fa2c82301664abb9bc3120ca51cea06e06f537fd
k2 <skrik2@outlook.com>
2026-01-26 19:08:21 +0800

    Update backup.sh

--- vps/tencent/global/backup.sh
+++ vps/tencent/global/backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir -p backup/config_file
+mkdir -p /home/momo/backup/config_file
 
 # config 
 ## zsh
-cp .zshrc /home/momo/backup/config_file/
-cp .zsh_envs /home/momo/backup/config_file/
-cp .zsh_aliases /home/momo/backup/config_file/
+cp /home/momo/.zshrc /home/momo/backup/config_file/
+cp /home/momo/.zsh_envs /home/momo/backup/config_file/
+cp /home/momo/.zsh_aliases /home/momo/backup/config_file/
 
 # ssh
 rsync -rv /home/momo/.ssh /home/momo/backup/config_file/ 
@@ -15,18 +15,15 @@ cp /etc/ssh/sshd_config.d/global.conf /home/momo/backup/config_file/
 rsync -rv /home/momo/.config /home/momo/backup/config_file/
 
 ## git
-cp .gitconfig /home/momo/backup/config_file/
+cp /home/momo/.gitconfig /home/momo/backup/config_file/
 
 # apps file dev
-sudo rsync -rv apps /home/momo/backup/
-rsync -rv file /home/momo/backup/ 
-sudo rsync -rv dev /home/momo/backup/ 
-
-mkdir temp
-mv backup temp && cd temp
+sudo rsync -rv /home/momo/apps /home/momo/backup/
+rsync -rv /home/momo/file /home/momo/backup/ 
+sudo rsync -rv /home/momo/dev /home/momo/backup/ 
 
 timestamp=$(date "+%Y%m%d%H%M")
 filename="backup_${timestamp}.zip"
-zip -s 256m -r ${filename} backup
+zip -r ${filename} /home/momo/backup
 
-rm -rf backup
+rm -rf /home/momo/backup
